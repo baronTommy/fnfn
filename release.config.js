6db981eb530b4315b611a85896e886f12d8448a5
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 16:44:06 +0900

    build: 👷 xxx

--- release.config.js
+++ release.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+
+          { type: "build", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "feat", release: "patch" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "test", release: "patch" },
+
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
   ],
-  branches: ['master'],
+  branches: ["master"],
 };
