b3b61535f4d53b1a68b54002e4e53dc6b636d199
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 21:33:10 +0900

    :sparkles: feat(changelog): add chinese title
    BREAKING CHANGE: 🧨 iii

--- release.config.js
+++ release.config.js
@@ -8,6 +8,7 @@ module.exports = {
         parserOpts: {
           headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
           headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
         },
       }
     ],
