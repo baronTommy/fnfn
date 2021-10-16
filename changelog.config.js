abcd138ac6eebcc3e70f763d79b83e0475bb093f
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 21:41:36 +0900

    📝 feat: xxx
    ccc
    
    ✅ Closes: #222

--- changelog.config.js
+++ changelog.config.js
@@ -56,7 +56,7 @@ const angularStyle = {
 
   module.exports = {
     disableEmoji: false,
-    format: `{emoji} {type}{scope}: {subject}`,
+    format: `{emoji}{type}{scope}: {subject}`,
     list: Object.keys(angularStyle),
     maxMessageLength: 64,
     minMessageLength: 3,
