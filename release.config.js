b97bb2b188207ccd353211f6443d8f2a4ee4036c
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 19:58:07 +0900

    :breaking: xxx

--- release.config.js
+++ release.config.js
@@ -4,24 +4,31 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer",
       {
-        releaseRules: [
-          // major
-          { type: "feat", release: "major" },
 
-          // minor
-          { type: "customValue1", release: "minor" },
+        "parserOpts": {
+          "headerPattern": "^:(\\w+): \\s(.*)$",
+          "headerCorrespondence": [ "type", "subject" ],
+        },
 
-          // patch
-          { type: "perf", release: "patch" },
-          { type: "fix", release: "patch" },
-          { type: "refactor", release: "patch" },
+        "releaseRules": [
+          {
+            "breaking": true,
+            "release": "major",
+          },
+        ]
 
+        // releaseRules: [
+        //   // major
+        //   { type: "feat", release: "major" },
 
-          // { type: "build", "release": false },
-          // { type: "ci", "release": false },
-          // { type: "docs", "release": false },
-          // { type: "test", "release": false },
-        ],
+        //   // minor
+        //   { type: "customValue1", release: "minor" },
+
+        //   // patch
+        //   { type: "perf", release: "patch" },
+        //   { type: "fix", release: "patch" },
+        //   { type: "refactor", release: "patch" },
+        // ],
       },
     ],
   ],
