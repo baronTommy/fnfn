264f8c85a014cfdad7d461257bef95113d524d73
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 20:37:39 +0900

    :art:feat(pencil): add 'graphiteWidth' option

--- release.config.js
+++ release.config.js
@@ -9,9 +9,8 @@ module.exports = {
         //   "headerCorrespondence": [ "type", "subject" ],
         // },
         parserOpts: {
-          headerPattern:
-            /^(?:(?:\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]) (\w*))(?:\(([\w\$\.\-\* ]*)\))?\: (.*)$/,
-            "headerCorrespondence": [ "type", "subject" ],
+          headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+          headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
         },
         releaseRules: [
           {
