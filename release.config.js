d69f60547e1f6ecc7752c0be2447edb191856829
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 21:30:17 +0900

    🐛 feat: xxx

--- release.config.js
+++ release.config.js
@@ -4,39 +4,12 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer",
       {
-        // 成功
-        // parserOpts: {
-        //   headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
-        //   headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
-        // },
-        
-
+        // :sparkles: feat(changelog): support chinese title
         parserOpts: {
-          "headerPattern": "^:(\\w+): \\s(.*)$",
-          "headerCorrespondence": [ "type", "subject" ],
+          headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+          headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
         },
-        "releaseRules": [
-          {
-            "type": "adhesive_bandage",
-            "release": "patch",
-          },
-          {
-            "type": ":adhesive_bandage:",
-            "release": "major",
-          },
-          {
-            "type": "adhesive_bandage:",
-            "release": "major",
-          },
-
-          {
-            "type": ":adhesive_bandage",
-            "release": "minor",
-          },
-        ]
-      },
+      }
     ],
   ],
 };
-
-
