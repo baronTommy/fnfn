83933c9c3f3fdd883f5d0fbc1fb9c78eb33655ef
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-16 20:54:11 +0900

    :adhesive_bandage: xxxxxxxx

--- release.config.js
+++ release.config.js
@@ -4,37 +4,36 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer",
       {
-        // "parserOpts": {
-        //   "headerPattern": "^:(\\w+): \\s(.*)$",
-        //   "headerCorrespondence": [ "type", "subject" ],
+        // 成功
+        // parserOpts: {
+        //   headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+        //   headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
         // },
+        
+
         parserOpts: {
-          headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
-          headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+          "headerPattern": "^:(\\w+): \\s(.*)$",
+          "headerCorrespondence": [ "type", "subject" ],
         },
-        // releaseRules: [
-        //   {
-        //     type: "art",
-        //     release: "major",
-        //   },
-        //   {
-        //     type: ":art:",
-        //     release: "patch",
-        //   },
-        // ],
+        "releaseRules": [
+          {
+            "type": "adhesive_bandage",
+            "release": "patch",
+          },
+          {
+            "type": ":adhesive_bandage:",
+            "release": "major",
+          },
+          {
+            "type": "adhesive_bandage:",
+            "release": "major",
+          },
 
-        // releaseRules: [
-        //   // major
-        //   { type: "feat", release: "major" },
-
-        //   // minor
-        //   { type: "customValue1", release: "minor" },
-
-        //   // patch
-        //   { type: "perf", release: "patch" },
-        //   { type: "fix", release: "patch" },
-        //   { type: "refactor", release: "patch" },
-        // ],
+          {
+            "type": ":adhesive_bandage",
+            "release": "minor",
+          },
+        ]
       },
     ],
   ],
