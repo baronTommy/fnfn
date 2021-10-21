e4d015ab6cd5d7f63dfe9493ba1caaaa0c4adf95
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-22 00:01:31 +0900

    ii

--- interactive-commit-message.js
+++ interactive-commit-message.js
@@ -1,3 +1,117 @@
+const { github, gitmojis } = require("@tommy_baron/git-test-").plugin;
+
+const gitmoji = gitmojis.map((v) => ({
+  name: `${v.emoji} ${v.description}`,
+  value: v.code,
+}));
+
+const notSelected = { name: "_NotSelected_", value: "" };
+
+/**
+ * https://github.com/octokit/rest.js/
+ */
+const fetchMyIssues = () =>
+  github
+    .fetchIssues({
+      // TODO リネーム
+      owner: "baronTommy",
+      repo: "test3",
+      state: "open",
+    })
+    .then((r) =>
+      r.data.map((issue) => ({
+        name: `#${issue.number}: ${issue.title}`,
+        value: `${issue.number}`,
+      }))
+    )
+    .then((v) => [notSelected, ...v])
+    .catch(() => []);
+
+/**
+ * @type {import('@tommy_baron/git-test-').Setting}
+ */
 module.exports = {
-    foo: 'bar'
-}
+  /**
+   * The part that matches questionDictionary.name will be replaced.
+   */
+  template: `{{type}}({{scope}}): {{gitmoji}} {{description}}
+          
+{{body}}
+             
+{{issue}}`,
+  config: {
+    /**
+     * This is the theme color of the terminal.
+     */
+    color: "green",
+  },
+  questionDictionary: [
+    {
+      name: "type",
+      type: "search-list",
+      message: "Please select a type.",
+      getChoices: () =>
+        Promise.resolve([
+          { name: "Fix: for a bug fix.", value: "Fix" },
+          {
+            name: "Update: either for a backwards-compatible enhancement or for a rule change that adds reported problems.",
+            value: "Update",
+          },
+          { name: "New: implemented a new feature.", value: "New" },
+          {
+            name: "Breaking: for a backwards-incompatible enhancement or feature.",
+            value: "Breaking",
+          },
+          { name: "Docs: changes to documentation only.", value: "Docs" },
+          { name: "Build: changes to build process only.", value: "Build" },
+          { name: "Upgrade: for a dependency upgrade.", value: "Upgrade" },
+          {
+            name: "Chore: for refactoring, adding tests, etc. (anything that isn't user-facing).",
+            value: "Chore",
+          },
+        ]),
+    },
+    {
+      name: "scope",
+      type: "search-list",
+      message: "Please select a scope.",
+      getChoices: () =>
+        Promise.resolve([
+          notSelected,
+          { name: "Interface", value: "Interface" },
+          { name: "UserInterface", value: "UserInterface" },
+          { name: "UseCase", value: "UseCase" },
+          { name: "Domain", value: "Domain" },
+        ]),
+      overwriteTpl: (tpl) => tpl.replace("()", ""),
+    },
+    {
+      name: "gitmoji",
+      type: "search-list",
+      message: "Please select a gitmoji",
+      getChoices: () => Promise.resolve(gitmoji),
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Please input the description.",
+    },
+    {
+      name: "body",
+      type: "input",
+      message: "Please input the body.",
+      overwriteTpl: (tpl) => tpl.replace(/\r?\n{2,}/g, "").trim(),
+    },
+    {
+      name: "issue",
+      type: "search-list",
+      message: "Close the issue?",
+      /**
+       * Get the issue.
+       */
+      getChoices: fetchMyIssues,
+      overwriteAnswer: (ans) => (ans ? `Close #${ans}` : ""),
+      overwriteTpl: (tpl) => tpl.replace(/\r?\n{2,}/g, "").trim(),
+    },
+  ],
+};
--- release.config.js
+++ release.config.js
@@ -5,21 +5,27 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         // https://github.com/arvinxx/gitmoji-commit-workflow
-        parserOpts: {
-          headerPattern:
-            /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
-          headerCorrespondence: ["type", "scope", "subject", "ticket"],
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-        },
+        // parserOpts: {
+        //   headerPattern:
+        //     /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+        //   headerCorrespondence: ["type", "scope", "subject", "ticket"],
+        //   noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        // },
         releaseRules: [
-          {
-            breaking: true,
-            release: "major",
-          },
-          { type: "feat", release: "minor" },
-          {
-            release: "patch",
-          },
+          // {
+          //   breaking: true,
+          //   release: "major",
+          // },
+          { type: "Breaking", release: "major" },
+
+          
+          { type: "New", release: "minor" },
+          
+          { type: "Fix", release: "patch" },
+          { type: "Update", release: "patch" },
+          // {
+          //   release: "patch",
+          // },
         ],
       },
     ],
