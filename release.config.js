723c6c83ad6750264d5527f216b0d5894d6b55c1
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-23 01:12:31 +0900

    Fix: :art: fi
    ho

--- release.config.js
+++ release.config.js
@@ -29,7 +29,7 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "standard-changelog",
+        preset: "angular",
         presetConfig: {
           types: [
             {
