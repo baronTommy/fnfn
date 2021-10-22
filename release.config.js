5ee3d6f9b598855df6f0a3da0e40da8cd4048743
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-23 00:22:25 +0900

    Fix: :art:

--- release.config.js
+++ release.config.js
@@ -29,7 +29,7 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "conventional-changelog-conventionalcommits",
+        preset: "conventional-changelog",
         presetConfig: {
           types: [
             {
