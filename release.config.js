2c79e5de084f98db52ca56ca1eeac15d004ac1f9
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-22 19:02:34 +0900

    feat: :art: feat

--- release.config.js
+++ release.config.js
@@ -1,7 +1,35 @@
+
 module.exports = {
   branches: ["master"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        // https://github.com/arvinxx/gitmoji-commit-workflow
+        // parserOpts: {
+        //   headerPattern:
+        //     /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+        //   headerCorrespondence: ["type", "scope", "subject", "ticket"],
+        //   noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        // },
+
+
+        "preset": "conventionalcommits",
+        releaseRules: [
+          // {
+          //   breaking: true,
+          //   release: "major",
+          // },
+          { type: "Breaking", release: "major" },
+          { type: "New", release: "minor" },
+          { type: "Fix", release: "patch" },
+          { type: "Update", release: "patch" },
+          // {
+          //   release: "patch",
+          // },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ]
