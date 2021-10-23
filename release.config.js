e4a03632e37b38df9c25094c85cf1815387d6c53
baronTommy <1675038-Tommy-baron@users.noreply.gitlab.com>
2021-10-23 20:25:18 +0900

    Fix: :art:

--- release.config.js
+++ release.config.js
@@ -4,31 +4,9 @@ module.exports = {
     [
       "@semantic-release/commit-analyzer",
       {
-        // https://github.com/arvinxx/gitmoji-commit-workflow
-        // parserOpts: {
-        //   headerPattern:
-        //     /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
-        //   headerCorrespondence: ["type", "scope", "subject", "ticket"],
-        //   noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
-        // },
-
         preset: "conventionalcommits",
         releaseRules: [
-          // {
-          //   breaking: true,
-          //   release: "major",
-          // },
-          // { type: "Breaking", release: "major" },
-          // { type: "New", release: "minor" },
-          // { type: "new", release: "minor" },
-          // { type: "Fix", release: "patch" },
-          // { type: "Update", release: "patch" },
-          // {
-          //   release: "patch",
-          // },
-
           { type: "Foo", release: "major" },
-
         ],
       },
     ],
@@ -36,30 +14,80 @@ module.exports = {
       "@semantic-release/release-notes-generator",
       {
         preset: "conventionalcommits",
-        // presetConfig: {
-        //   types: [
-        //     {
-        //       type: "Update",
-        //       section: ":sparkles: Novidades",
-        //       hidden: false,
-        //     },
-        //     {
-        //       type: "New",
-        //       section: ":sparkles: Novidades",
-        //       hidden: false,
-        //     },
-        //     {
-        //       type: "Fix",
-        //       section: ":bug: Correções",
-        //       hidden: false,
-        //     },
-        //   ],
-        // },
+        "presetConfig": {
+          "types": [
+            {"type": "Foo", "section": "Improvement", "hidden": false}
+          ]
       },
     ],
     "@semantic-release/github",
   ],
 };
+
+
+
+// module.exports = {
+//   branches: ["master"],
+//   plugins: [
+//     [
+//       "@semantic-release/commit-analyzer",
+//       {
+//         // https://github.com/arvinxx/gitmoji-commit-workflow
+//         // parserOpts: {
+//         //   headerPattern:
+//         //     /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
+//         //   headerCorrespondence: ["type", "scope", "subject", "ticket"],
+//         //   noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+//         // },
+
+//         preset: "conventionalcommits",
+//         releaseRules: [
+//           // {
+//           //   breaking: true,
+//           //   release: "major",
+//           // },
+//           // { type: "Breaking", release: "major" },
+//           // { type: "New", release: "minor" },
+//           // { type: "new", release: "minor" },
+//           // { type: "Fix", release: "patch" },
+//           // { type: "Update", release: "patch" },
+//           // {
+//           //   release: "patch",
+//           // },
+
+//           { type: "Foo", release: "major" },
+
+//         ],
+//       },
+//     ],
+//     // [
+//     //   "@semantic-release/release-notes-generator",
+//     //   {
+//     //     preset: "conventionalcommits",
+//     //     // presetConfig: {
+//     //     //   types: [
+//     //     //     {
+//     //     //       type: "Update",
+//     //     //       section: ":sparkles: Novidades",
+//     //     //       hidden: false,
+//     //     //     },
+//     //     //     {
+//     //     //       type: "New",
+//     //     //       section: ":sparkles: Novidades",
+//     //     //       hidden: false,
+//     //     //     },
+//     //     //     {
+//     //     //       type: "Fix",
+//     //     //       section: ":bug: Correções",
+//     //     //       hidden: false,
+//     //     //     },
+//     //     //   ],
+//     //     // },
+//     //   },
+//     // ],
+//     "@semantic-release/github",
+//   ],
+// };
 
 // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-commits-parser#conventionalcommitsparseroptions
 // https://github.com/semantic-release/release-notes-generator/issues/153#issuecomment-555152563
